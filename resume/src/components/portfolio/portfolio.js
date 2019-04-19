1518f4d53b8a0712397f7a2b348f22a40dfbd8e5
Montique Stevens <mstevens528@gmail.com>
2019-04-19 19:53:40 -0400

    Various UI changes

--- resume/src/components/portfolio/portfolio.js
+++ resume/src/components/portfolio/portfolio.js
@@ -15,7 +15,7 @@ export default class Porfolio extends Component {
                 <ol className="type">
                   <li><a href="#" data-filter="*" className="active">All</a></li>
                   <li><a href="#" data-filter=".web">Concepts</a></li>
-                  <li><a href="#" data-filter=".app">Blogging</a></li>
+                  <li><a href="#" data-filter=".app">Projects</a></li>
                 </ol>
               </li>
             </ul>
@@ -32,7 +32,7 @@ export default class Porfolio extends Component {
                       </div>
                       <img src="img/example/01-small.jpg" className="img-responsive" alt="Tribute Site" />
                     </a> </div>
-                  <a data-v-4b155e36 href="https://github.com/nycbeardo/material-design-card" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
+                  <a data-v-4b155e36 href="https://github.com/nycbeardo/personalproject" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                     </svg> <span data-v-4b155e36>View source on GitHub</span></a>
                 </div>
@@ -41,12 +41,12 @@ export default class Porfolio extends Component {
                 <div className="portfolio-item">
                   <div className="hover-bg"> <a href="img/example/02-large.jpg" title="Project description" rel="prettyPhoto">
                       <div className="hover-text">
-                        <h4>Project Title</h4>
+                        <h4>Tic Tac Toe</h4>
                         <small>App Development</small>
                       </div>
                       <img src="img/example/02-small.jpg" className="img-responsive" alt="Project Title" />
                     </a> </div>
-                  <a data-v-4b155e36 href="https://github.com/nycbeardo/material-design-card" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
+                  <a data-v-4b155e36 href="https://github.com/nycbeardo/TicTacToe" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                     </svg> <span data-v-4b155e36>View source on GitHub</span></a>
                 </div>
@@ -69,12 +69,12 @@ export default class Porfolio extends Component {
                 <div className="portfolio-item">
                   <div className="hover-bg"> <a href="img/example/04-large.jpg" title="Project description" rel="prettyPhoto">
                       <div className="hover-text">
-                        <h4>Project Title</h4>
+                        <h4>Digital Clock</h4>
                         <small>Web Design</small>
                       </div>
                       <img src="img/example/04-small.jpg" className="img-responsive" alt="Project Title" />
                     </a> </div>
-                  <a data-v-4b155e36 href="https://github.com/nycbeardo/material-design-card" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
+                  <a data-v-4b155e36 href="https://github.com/nycbeardo/Digital-Clock" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                     </svg> <span data-v-4b155e36>View source on GitHub</span></a>
                 </div>
@@ -83,12 +83,12 @@ export default class Porfolio extends Component {
                 <div className="portfolio-item">
                   <div className="hover-bg"> <a href="img/example/05-large.jpg" title="Project description" rel="prettyPhoto">
                       <div className="hover-text">
-                        <h4>Project Title</h4>
+                        <h4>Issue Tracker</h4>
                         <small>App Development</small>
                       </div>
                       <img src="img/example/05-small.jpg" className="img-responsive" alt="Project Title" />
                     </a> </div>
-                  <a data-v-4b155e36 href="https://github.com/nycbeardo/material-design-card" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
+                  <a data-v-4b155e36 href="https://github.com/nycbeardo/Javascript-Project" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                     </svg> <span data-v-4b155e36>View source on GitHub</span></a>
                 </div>
@@ -111,12 +111,12 @@ export default class Porfolio extends Component {
                 <div className="portfolio-item">
                   <div className="hover-bg"> <a href="img/example/07-large.jpg" title="Project description" rel="prettyPhoto">
                       <div className="hover-text">
-                        <h4>Project Title</h4>
-                        <small>App Development, Branding</small>
+                        <h4>To Do List</h4>
+                        <small>App Development</small>
                       </div>
                       <img src="img/example/07-small.jpg" className="img-responsive" alt="Project Title" />
                     </a> </div>
-                  <a data-v-4b155e36 href="https://github.com/nycbeardo/material-design-card" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
+                  <a data-v-4b155e36 href="https://github.com/nycbeardo/To-Do-List" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                     </svg> <span data-v-4b155e36>View source on GitHub</span></a>
                 </div>
