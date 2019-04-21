77abf7212ade20b82aae3634dff286172afab2bc
Montique Stevens <mstevens528@gmail.com>
2019-04-21 19:26:24 -0400

    Misc Fixes
    New Preview img added

--- resume/src/components/portfolio/portfolio.js
+++ resume/src/components/portfolio/portfolio.js
@@ -25,14 +25,14 @@ export default class Porfolio extends Component {
             <div className="portfolio-items">
               <div className="col-sm-6 col-md-3 col-lg-3 web">
                 <div className="portfolio-item">
-                  <div className="hover-bg"> <a href="img/example/01-large.jpg" title="Example of a tribute Site for the Worlds Fair in Queens" rel="prettyPhoto">
+                  <div className="hover-bg"> <a href="img/example/01-large.jpg" title="This is a technical documentation page concept, created with CSS and Node.js" rel="prettyPhoto">
                       <div className="hover-text">
-                        <h4>Tribute Site</h4>
+                        <h4>Technical Documentation Page</h4>
                         <small>Web Design</small>
                       </div>
                       <img src="img/example/01-small.jpg" className="img-responsive" alt="Tribute Site" />
                     </a> </div>
-                  <a data-v-4b155e36 href="https://github.com/nycbeardo/personalproject" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
+                  <a data-v-4b155e36 href="https://github.com/nycbeardo/Technical-Doc-Page" className="project__source"><svg data-v-4b155e36 xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github" data-darkreader-inline-fill data-darkreader-inline-stroke style={{-darkreaderInlineFill: 'none', -darkreaderInlineStroke: 'currentColor'}}>
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                     </svg> <span data-v-4b155e36>View source on GitHub</span></a>
                 </div>
