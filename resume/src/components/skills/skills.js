50ad9327997123c25f66e4e920837b575c10b494
Montique Stevens <mstevens528@gmail.com>
2019-05-01 04:28:40 -0400

    tech stack mod
    added python to skillset list

--- resume/src/components/skills/skills.js
+++ resume/src/components/skills/skills.js
@@ -17,16 +17,16 @@ export default  class Skills extends Component {
               <h4>CSS3 <i className="devicon-css3-plain colored" style={{fontSize: '60px'}} /></h4>
             </div>
             <div className="col-md-4 col-sm-6 skill">
-              <h4>jQuery <i className="devicon-jquery-plain colored" style={{fontSize: '60px'}} /></h4>
+            <h4>Javascript <i class="devicon-javascript-plain colored" style="font-size:60px;"></i></h4>
             </div>
             <div className="col-md-4 col-sm-6 skill">
               <h4>Bootstrap <i className="devicon-bootstrap-plain colored" style={{fontSize: '60px'}} /></h4>
             </div>
             <div className="col-md-4 col-sm-6 skill">
-              <h4>Javascript <i className="devicon-javascript-plain colored" style={{fontSize: '60px'}} /></h4>
+            <h4>React<i class="devicon-react-plain colored" style="font-size:60px;"></i></h4>
             </div>
             <div className="col-md-4 col-sm-6 skill">
-              <h4>Wordpress <i className="devicon-wordpress-plain colored" style={{fontSize: '60px'}} /></h4>
+              <h4>Python <i className="devicon-python-plain colored" style={{fontSize: '60px'}} /></h4>
             </div>
             <div className="col-md-4 col-sm-6 skill">
               <h4>C++ <i className="devicon-cplusplus-plain colored" style={{fontSize: '60px'}} /></h4>
